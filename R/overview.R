73b0b184e523679a14250d19b521131f67005488
Robert J. Calin-Jageman <rcalinjageman@gmail.com>
2021-10-18 22:00:58 -0500

    overview work

--- R/overview.R
+++ R/overview.R
@@ -15,20 +15,37 @@ overview.base <- function(
   contrasts <- matrix(data = 0, nrow = n_means, ncol = n_means)
   diag(contrasts) <- 1
 
-  statpsych_row_number <- if(assume_equal_variance) 1 else 2
-  statpsych_column_names <- c("LL", "UL", "df", "SE")
+  statpsych_row <- if (assume_equal_variance | n_means == 1) 1 else 2
+  statpsych_cnames <- c("LL", "UL", "df", "SE")
   esci_column_names <- c("mean_LL", "mean_UL", "df", "mean_SE")
 
-for (x in 1:n_means) {
-    res <- statpsych::ci.lc.mean.bs(
-      alpha = 1 - conf_level,
-      m = overview_table$mean,
-      s = overview_table$sd,
-      n = overview_table$n,
-      v = contrasts[x,]
+  if (n_means == 1) {
+    res <- as.data.frame(
+        statpsych::ci.mean1(
+        alpha = 1 - conf_level,
+        m = overview_table$mean,
+        sd = overview_table$sd,
+        n = overview_table$n
+      )
     )
 
-    overview_table[x, esci_column_names] <- res[statpsych_row_number, statpsych_column_names]
+    res$df <- overview_table$n - 1
+
+    overview_table[1, esci_column_names] <- res[1, statpsych_cnames]
+
+  } else {
+
+    for (x in 1:n_means) {
+      res <- statpsych::ci.lc.mean.bs(
+        alpha = 1 - conf_level,
+        m = overview_table$mean,
+        s = overview_table$sd,
+        n = overview_table$n,
+        v = contrasts[x,]
+      )
+
+      overview_table[x, esci_column_names] <- res[statpsych_row, statpsych_cnames]
+    }
 
   }
 
@@ -106,9 +123,11 @@ overview.vector <- function(
   assume_equal_variance = TRUE
 ) {
 
+  # If no grouping variable, just summarize whole vector
   if (is.null(grouping_variable)) {
     grouping_variable <- rep("All", length(outcome_variable))
   }
+
 
   # Deal with NA values in grouping variable------------------------------------
   grouping_variable_report <- esci_assert_vector_valid_length(
@@ -120,71 +139,116 @@ overview.vector <- function(
   grouping_variable_missing <- grouping_variable_report$missing
 
 
-  # Calculate overview ---------------------------------------------------------
+  # Get grouping variable levels and outcome variable name ---------------------
   groups <- levels(addNA(grouping_variable))
 
-  overview <- overview.table_definition(rows = length(groups))
-  overview$group = groups
-  overview$m <- aggregate(
+  outcome_variable_name <- deparse(substitute(outcome_variable))
+  outcome_variable_names <- rep(outcome_variable_name, length(groups))
+
+
+  # Build the overview table ---------------------------------------------------
+  overview_table <- data.frame(
+    outcome_variable_name = outcome_variable_names,
+    grouping_variable_levels = groups
+  )
+
+  overview_table$mean <- aggregate(
     outcome_variable,
     by = list(addNA(grouping_variable)),
     FUN = mean,
     drop = FALSE,
     na.rm = TRUE)[, 2]
-  overview[ , c("median", "q1", "q3")] <-aggregate(
+
+  overview_table$mean_LL <- NA
+
+  overview_table$mean_UL <- NA
+
+  overview_table[ , c("median", "median_SE_temp", "median_LL", "median_UL")] <- aggregate(
+    outcome_variable,
+    by = list(addNA(grouping_variable)),
+    FUN = wrap_ci_median1,
+    drop = FALSE,
+    conf_level = conf_level,
+    na.rm = TRUE
+  )[, 2]
+
+  overview_table$median <-aggregate(
     outcome_variable,
     by = list(addNA(grouping_variable)),
     FUN = quantile,
     drop = FALSE,
-    probs = c(0.50, 0.25, 0.75),
+    probs = c(0.50),
     na.rm = TRUE)[, 2]
-  overview$s <- aggregate(
+
+  overview_table$sd <- aggregate(
     outcome_variable,
     by = list(addNA(grouping_variable)),
     FUN = sd,
     drop = FALSE,
     na.rm = TRUE)[, 2]
-  overview$min <- aggregate(
+
+  overview_table$min <- aggregate(
     outcome_variable,
     by = list(addNA(grouping_variable)),
     FUN = min,
     drop = FALSE,
     na.rm = TRUE)[, 2]
-  overview$max <- aggregate(
+
+  overview_table$max <- aggregate(
     outcome_variable,
     by = list(addNA(grouping_variable)),
     FUN = max,
     drop = FALSE,
     na.rm = TRUE)[, 2]
-  overview$n <- aggregate(
+
+  overview_table[ , c("q1", "q3")] <-aggregate(
+    outcome_variable,
+    by = list(addNA(grouping_variable)),
+    FUN = quantile,
+    drop = FALSE,
+    probs = c(0.25, 0.75),
+    na.rm = TRUE)[, 2]
+
+  overview_table$n <- aggregate(
     outcome_variable,
     by = list(addNA(grouping_variable)),
     drop = FALSE,
     FUN = length)[, 2]
-  overview$missing <-aggregate(
+
+  overview_table$missing <-aggregate(
     outcome_variable,
     by = list(addNA(grouping_variable)),
     drop = FALSE,
     function(x) { sum(is.na(x)) })[, 2]
-  overview$n <- overview$n - overview$missing
 
-  if (nrow(overview[is.na(overview$n), ]) > 0) {
-    overview[is.na(overview$n), ]$n <- 0
+  overview_table$n <- overview_table$n - overview_table$missing
+
+  overview_table$df <- NA
+
+  overview_table$mean_SE <- NA
+
+  overview_table$median_SE <- overview_table$median_SE_temp
+  overview_table$median_SE_temp <- NULL
+
+  # Now deal with invalid rows and missing data rows-----------------
+
+  if (nrow(overview_table[is.na(overview_table$n), ]) > 0) {
+    overview_table[is.na(overview_table$n), ]$n <- 0
   }
 
   na_level <- NULL
-  if(overview[nrow(overview), "n"] == 0) {
-    overview <- head(overview, -1)
+  if(overview_table[nrow(overview_table), "n"] == 0) {
+    overview_table <- head(overview_table, -1)
   } else {
     na_level <- "Missing"
-    while (na_level %in% overview$group) {
+    while (na_level %in% overview_table$group) {
       na_level <- paste(na_level, "*", sep ="")
     }
-    overview[nrow(overview), "group"] <- na_level
-    row.names(overview)[nrow(overview)] <- "missing"
+    overview_table[nrow(overview_table), "grouping_variable_level"] <- na_level
+    row.names(overview_table)[nrow(overview_table)] <- "missing"
   }
 
-  overview_no_miss <- overview[row.names(overview) != 'missing', ]
+  overview_no_miss <- overview_table[row.names(overview_table) != 'missing', ]
   overview_valid <- overview_no_miss[overview_no_miss$n > 1, ]
 
   overview_valid <- overview.base(
@@ -195,8 +259,8 @@ overview.vector <- function(
 
   overview_all <- rbind(
     overview_valid,
-    overview[row.names(overview) != "missing" & overview$n < 2, ],
-    overview[row.names(overview) == "missing", ]
+    overview_table[row.names(overview_table) != "missing" & overview_table$n < 2, ],
+    overview_table[row.names(overview_table) == "missing", ]
   )
 
   return(overview_all)
@@ -235,28 +299,17 @@ overview.list <- function(
 }
 
 
-# Base definition for the overview table
-overview.table_definition <- function(rows = 1) {
-  overview <- data.frame(
-    outcome_variable_name = rep(NA, times = rows),
-    grouping_variable_level = rep(NA, times = rows),
-    mean = rep(NA, times = rows),
-    mean_LL = rep(NA, times = rows),
-    mean_UL = rep(NA, times = rows),
-    median = rep(NA, times = rows),
-    median_LL = rep(NA, times = rows),
-    median_UL = rep(NA, times = rows),
-    sd = rep(NA, times = rows),
-    min = rep(NA, times = rows),
-    max = rep(NA, times = rows),
-    q1 = rep(NA, times = rows),
-    q3 = rep(NA, times = rows),
-    n = rep(NA, times = rows),
-    missing = rep(NA, times = rows),
-    df = rep(NA, times = rows),
-    sp = rep(NA, times = rows),
-    mean_se = rep(NA, times = rows),
-    median_se = rep(NA, times = rows),
+wrap_ci_median1 <- function(
+  x,
+  conf_level,
+  na.rm = TRUE,
+  drop = FALSE
+) {
+
+  res <- statpsych::ci.median1(
+    alpha = 1 - conf_level,
+    y = if(na.rm) x[!is.na(x)] else x
   )
-  return(overview)
+
+  return(res)
 }
