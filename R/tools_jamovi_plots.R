c23d075deccaa7fbf364e0eeb30ce33f05c9ec8a
Robert J Calin-Jageman <rcalinjageman@gmail.com>
2022-03-26 07:53:58 -0500

    mdiffone figure in jamovi

--- R/tools_jamovi_plots.R
+++ R/tools_jamovi_plots.R
@@ -110,7 +110,160 @@ jamovi_plot_mdiff <- function(
     args = args
   )
 
-  self$results$estimation_plot_warnings$setState(notes)
+  # Basic graph options --------------------
+  # Axis font sizes
+  axis.text.y <- jamovi_sanitize(
+    my_value = self$options$axis.text.y,
+    return_value = 10,
+    na_ok = FALSE,
+    convert_to_number = TRUE,
+    lower = 1,
+    lower_inclusive = TRUE,
+    my_value_name = "Y axis: Tick font size"
+  )
+  axis.title.y <- jamovi_sanitize(
+    my_value = self$options$axis.title.y,
+    return_value = 12,
+    na_ok = FALSE,
+    convert_to_number = TRUE,
+    lower = 1,
+    lower_inclusive = TRUE,
+    my_value_name = "Y axis: Label font size"
+  )
+  axis.text.x <- jamovi_sanitize(
+    my_value = self$options$axis.text.x,
+    return_value = 10,
+    na_ok = FALSE,
+    convert_to_number = TRUE,
+    lower = 1,
+    lower_inclusive = TRUE,
+    my_value_name = "X axis: Tick font size"
+  )
+  axis.title.x <- jamovi_sanitize(
+    my_value = self$options$axis.title.x,
+    return_value = 10,
+    na_ok = FALSE,
+    convert_to_number = TRUE,
+    lower = 1,
+    lower_inclusive = TRUE,
+    my_value_name = "X axis: Label font size"
+  )
+
+
+  myplot <- myplot + ggplot2::theme(
+    axis.text.y = element_text(size = axis.text.y),
+    axis.title.y = element_text(size = axis.title.y),
+    axis.text.x = element_text(size = axis.text.x),
+    axis.title.x = element_text(size = axis.title.x)
+  )
+
+
+  # Axis labels
+  xlab <- jamovi_sanitize(
+    my_value = self$options$xlab,
+    return_value = NULL,
+    na_ok = FALSE,
+    my_value_name = "X axis: Title"
+  )
+
+  ylab <- jamovi_sanitize(
+    my_value = self$options$ylab,
+    return_value = NULL,
+    na_ok = FALSE,
+    my_value_name = "Y axis: Title"
+  )
+
+  if (!is.null(xlab)) {
+    myplot <- myplot + ggplot2::xlab(xlab)
+  }
+  if (!is.null(ylab)) {
+    myplot <- myplot + ggplot2::ylab(ylab)
+  }
+
+  # Aesthetics
+  myplot <- myplot + ggplot2::scale_shape_manual(
+    values = c(
+      "Reference_raw" = self$options$shape_raw_reference,
+      "Comparison_raw" = self$options$shape_raw_comparison,
+      "Difference_raw" = "triangle filled",
+      "Unused_raw" = "diamond filled",
+      "Reference_summary" = self$options$shape_summary_reference,
+      "Comparison_summary" = self$options$shape_summary_comparison,
+      "Difference_summary" = self$options$shape_summary_difference,
+      "Unused_summary" = "circle filled"
+    )
+  )
+
+  myplot <- myplot + ggplot2::scale_color_manual(
+    values = c(
+      "Reference_raw" = self$options$color_raw_reference,
+      "Comparison_raw" = self$options$color_raw_comparison,
+      "Difference_raw" = "black",
+      "Unused_raw" = "black",
+      "Reference_summary" = self$options$color_summary_reference,
+      "Comparison_summary" = self$options$color_summary_comparison,
+      "Difference_summary" = self$options$color_summary_difference,
+      "Unused_summary" = "gray80"
+    ),
+    aesthetics = c("color", "point_color")
+  )
+
+  myplot <- myplot + ggplot2::scale_fill_manual(
+    values = c(
+      "Reference_raw" = self$options$fill_raw_reference,
+      "Comparison_raw" = self$options$fill_raw_comparison,
+      "Difference_raw" = "NA",
+      "Unused_raw" = "gray80",
+      "Reference_summary" = self$options$fill_summary_reference,
+      "Comparison_summary" = self$options$fill_summary_comparison,
+      "Difference_summary" = self$options$fill_summary_difference,
+      "Unused_summary" = "gray80"
+    ),
+    aesthetics = c("fill", "point_fill")
+  )
+
+  myplot <- myplot + ggplot2::discrete_scale(
+    c("size", "point_size"),
+    "point_size_d",
+    function(n) return(c(
+      "Reference_raw" = as.integer(self$options$size_raw_reference),
+      "Comparison_raw" = as.integer(self$options$size_raw_comparison),
+      "Difference_raw" = 2,
+      "Unused_raw" = 1,
+      "Reference_summary" = as.integer(self$options$size_summary_reference),
+      "Comparison_summary" = as.interger(self$options$size_summary_comparison),
+      "Difference_summary" = as.integer(self$options$size_summary_difference),
+      "Unused_summary" = 3
+    ))
+  )
+
+  myplot <- myplot + ggplot2::discrete_scale(
+    c("alpha", "point_alpha"),
+    "point_alpha_d",
+    function(n) return(c(
+      "Reference_raw" = as.number(self$options$alpha_raw_reference),
+      "Comparison_raw" = .as.number(self$options$alpha_raw_comparison),
+      "Difference_raw" = .8,
+      "Unused_raw" = .5,
+      "Reference_summary" = as.number(self$options$alpha_summary_reference),
+      "Comparison_summary" = as.number(self$options$alpha_summary_comparison),
+      "Difference_summary" = as.number(self$options$alpha_summary_difference),
+      "Unused_summary" = 1
+    ))
+  )
+
+
+  self$results$estimation_plot_warnings$setState(
+    c(
+      notes,
+      names(xlab),
+      names(ylab),
+      names(axis.text.y),
+      names(axis.title.y),
+      names(axis.text.x),
+      names(axis.title.x)
+    )
+  )
   jamovi_set_notes(self$results$estimation_plot_warnings)
 
   return(myplot)
