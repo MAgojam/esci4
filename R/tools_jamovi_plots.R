6157b31592feb3def98d49432b0308cf8ec51631
Robert J. Calin-Jageman <rcalinjageman@gmail.com>
2022-08-21 21:33:26 -0500

    htest

--- R/tools_jamovi_plots.R
+++ R/tools_jamovi_plots.R
@@ -467,6 +467,21 @@ jamovi_plot_hdiff <- function(
   # Basic plot
   notes <- NULL
 
+  myplot$layers[[3]]$aes_params$shape <- self$options$shape_summary_difference
+  myplot$layers[[3]]$aes_params$colour <- self$options$color_summary_difference
+  myplot$layers[[3]]$aes_params$fill <- self$options$fill_summary_difference
+  myplot$layers[[3]]$aes_params$alpha <- as.numeric(self$options$alpha_summary_difference)
+  myplot$layers[[3]]$aes_params$size <- as.numeric(self$options$size_summary_difference)
+
+  myplot$layers[[1]]$aes_params$linetype <- self$options$linetype_summary_difference
+  myplot$layers[[2]]$aes_params$linetype <- self$options$linetype_summary_difference
+  myplot$layers[[1]]$aes_params$colour <- self$options$color_interval_difference
+  myplot$layers[[2]]$aes_params$colour <- self$options$color_interval_difference
+  myplot$layers[[1]]$aes_params$alpha <- as.numeric(self$options$alpha_interval_difference)
+  myplot$layers[[2]]$aes_params$alpha <- as.numeric(self$options$alpha_interval_difference)
+  myplot$layers[[1]]$aes_params$size <- as.numeric(self$options$size_interval_difference)+2
+  myplot$layers[[2]]$aes_params$size <- as.numeric(self$options$size_interval_difference)
+
 
   # Basic graph options --------------------
   # Axis font sizes
